8100742f6622d6d4422d4f10711c0e0e3b500070
TagerTi <timon.reis08@gmail.com>
2026-05-12 08:41:17 +0200

    Added assets and new tower scene

---
+++ scenes/tower/Tower.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Tower : StaticBody2D
+{
+	private readonly List<Node2D> projectiles = [];
+
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+}
