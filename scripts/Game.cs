23fdd08118d22deea1eb7aeb3652aa386a85cad2
Monstroxx <jonas.boos@gmx.de>
2026-05-13 09:29:34 +0200

    dude der sich im kreis dreht

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,17 +1,27 @@
 using Godot;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class Game : Node2D
 {
-    public static int Coins = 10;
-
+	public static int Coins = 10;
+	PathFollow2D path_follow_node;
+	Node currentScene;
 	public override void _Ready()
-    {
-        
-    }
-    
-    public override void _Process(double delta)
-    {
-        
-    }
+	{
+		path_follow_node = GetNode<PathFollow2D>("Path2D/PathFollow2D");
+		currentScene = GetTree().CurrentScene;
+	}
+	
+	public override void _Process(double delta)
+	{
+		GetNode<PathFollow2D>("Path2D/PathFollow2D").Progress += 250 * (float)delta;
+
+	}
+	void spawn_at_point(PathFollow2D path_follow_node, PackedScene scene_to_spawn)
+	{
+		Node2D instance = (Node2D)scene_to_spawn.Instantiate();
+		GetParent().AddChild(instance);
+		instance.GlobalPosition = path_follow_node.GlobalPosition;
+	}
 }
