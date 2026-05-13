b34a7e16aae1143fcd35cbb0df86113029f48fab
TagerTi <timon.reis08@gmail.com>
2026-05-13 08:59:50 +0200

    Some simple scripts for starting

--- scripts/Enemie.cs
+++ scripts/Enemie.cs
@@ -3,11 +3,17 @@ using System;
 
 public partial class Enemie : Area2D
 {
+	public Path2D path;
+	private int nextTarget = 0;
+
+	public float speed = 1;
+
 	public override void _Ready()
 	{
 	}
 
 	public override void _Process(double delta)
 	{
+		
 	}
 }
